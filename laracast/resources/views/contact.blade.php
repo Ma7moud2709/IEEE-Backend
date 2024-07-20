04a9ff65c953e95022aa60d1f3f9fdff09769183
Mahmoud Said <mahmoueddragon2004@gmail.com>
2024-07-20 11:14:40 +0300

    Delete laracast directory

--- laracast/resources/views/contact.blade.php
+++
@@ -1,8 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Contact Page
-    </x-slot:heading>
-    <ul>
-        <h1 class="text-center">Contact Page</h1>
-    </ul>
-</x-layout>
--- laracast/resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Home Page
-    </x-slot:heading>
-    <h1 class="text-center ">Home Page</h1>
-
-</x-layout>
